963f280faf5e6348e4193ec9553ace158278d2a2
Ümitcan Tutar <umitcan237@gmail.com>
2023-05-04 20:55:47 +0300

    Squashed commit of the following:
    commit 0a62d9a0adfb21e8bd8af17590783c818a2d4242
    Merge: 525b85a ce8d389
    Author: Ümitcan Tutar <umitcan237@gmail.com>
    Date:   Thu May 4 20:53:22 2023 +0300
    
        Merge branch 'Bos_isler_müdürü'
    
    commit 525b85aece567533a6ea07a194a06999c9850334
    Author: Ümitcan Tutar <umitcan237@gmail.com>
    Date:   Thu May 4 20:48:41 2023 +0300
    
        deneme
    
    commit 9accb049753868d424b4ad6c88992d7485af22be
    Author: Ümitcan Tutar <umitcan237@gmail.com>
    Date:   Thu May 4 20:43:53 2023 +0300
    
        Squashed commit of the following:
    
        commit ce8d389df6370888b4f804a9bf6130c7a99ff33d
        Author: Kemal <k.duru51@gmail.com>
        Date:   Thu May 4 20:41:03 2023 +0300
    
            Başlıyoruz..

--- BinaryDatabase/src/BinaryDatabase/DatabaseGUI.java
+++ BinaryDatabase/src/BinaryDatabase/DatabaseGUI.java
@@ -1417,7 +1417,7 @@ public class DatabaseGUI {
 		btnInsertTable.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-				JFileChooser j = new JFileChooser("..\\Database");
+				JFileChooser j = new JFileChooser("..\\DbMeow");
 
 				// only show bin files
 				j.setFileFilter(new FileNameExtensionFilter("Binary Files", "bin"));
